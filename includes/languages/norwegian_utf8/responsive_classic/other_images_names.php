10b1881ae13be70481c6aca8e1e296cc7beffffb
syntaxerror <syntaxerror@users.noreply.github.com>
2016-05-23 17:17:41 +0200

    Nye for 1.5.5

---
+++ includes/languages/norwegian_utf8/responsive_classic/other_images_names.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: picaflor-azul Sun Dec 13 16:46:52 2015 -0500 New in v1.5.5 $
+ */
+
+/*
+ * define the other images used in the project
+ */
+
+define('OTHER_IMAGE_BLACK_SEPARATOR', 'pixel_black.gif');
+define('OTHER_IMAGE_BOX_NOTIFY_REMOVE', 'box_products_notifications_remove.gif');
+define('OTHER_IMAGE_BOX_NOTIFY_YES', 'box_products_notifications.gif');
+define('OTHER_IMAGE_BOX_WRITE_REVIEW', 'box_write_review.gif');
+define('OTHER_IMAGE_CALL_FOR_PRICE', 'call_for_prices.png');
+define('OTHER_IMAGE_DOWN_FOR_MAINTENANCE', 'down_for_maintenance.png');
+define('OTHER_IMAGE_PRICE_IS_FREE', 'free.png');
+define('OTHER_IMAGE_TRANPARENT', 'pixel_trans.gif');
+define('OTHER_IMAGE_CUSTOMERS_AUTHORIZATION', 'customer_authorization.gif');
+define('OTHER_IMAGE_REVIEWS_RATING_STARS_FIVE', 'stars_5_small.png');
+define('OTHER_IMAGE_REVIEWS_RATING_STARS_FOUR', 'stars_4_small.png');
+define('OTHER_IMAGE_REVIEWS_RATING_STARS_THREE', 'stars_3_small.png');
+define('OTHER_IMAGE_REVIEWS_RATING_STARS_TWO', 'stars_2_small.png');
+define('OTHER_IMAGE_REVIEWS_RATING_STARS_ONE', 'stars_1_small.png');
+//define('OTHER_IMAGE_', '');
+
+/*
+ * ALT tags for other images
+ */
+
+define('OTHER_BOX_NOTIFY_REMOVE_ALT', 'Fjern denne produktvarslingen.');
+define('OTHER_BOX_NOTIFY_YES_ALT', 'Varsle meg om oppdateringer på dette produktet.');
+define('OTHER_BOX_WRITE_REVIEW_ALT', 'Skriv en omtale for dette produktet.');
+define('OTHER_CALL_FOR_PRICE_ALT', 'Vennligst ring oss for oppdatert pris.');
+define('OTHER_DOWN_FOR_MAINTENANCE_ALT', 'Siden er for øyeblikket nede for vedlikehold. Velkommen tilbake litt senere.');
+define('OTHER_TRANPARENT_ALT', '');
+define('OTHER_IMAGE_CUSTOMERS_AUTHORIZATION_ALT', 'VENTER PÅ GODKJENNING AV KUNDEKONTO ...');
+define('OTHER_REVIEWS_RATING_STARS_FIVE_ALT', 'Fem stjerner');
+define('OTHER_REVIEWS_RATING_STARS_FOUR_ALT', 'Fire stjerner');
+define('OTHER_REVIEWS_RATING_STARS_THREE_ALT', 'Tre stjerner');
+define('OTHER_REVIEWS_RATING_STARS_TWO_ALT', 'To stjerner');
+define('OTHER_REVIEWS_RATING_STARS_ONE_ALT', 'En stjerne');
+//define('OTHER_xxx_ALT', '');
