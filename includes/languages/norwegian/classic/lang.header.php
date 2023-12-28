f6c853e719de7d559ad22250575ab9222941c6f3
syntaxerror <syntaxerror@users.noreply.github.com>
2023-12-28 18:40:20 +0200

    Removed deprecated strings

--- includes/languages/norwegian/classic/lang.header.php
+++ includes/languages/norwegian/classic/lang.header.php
@@ -1,14 +1,12 @@
 <?php
 $define = [
-    'HEADER_TITLE_CREATE_ACCOUNT' => 'Opprett konto',
     'HEADER_TITLE_MY_ACCOUNT' => 'Min konto',
     'HEADER_TITLE_CART_CONTENTS' => 'Handlekurv',
     'HEADER_TITLE_CHECKOUT' => 'Kasse',
-    'HEADER_TITLE_TOP' => 'Topp',
     'HEADER_TITLE_CATALOG' => 'Hjem',
     'HEADER_TITLE_LOGOFF' => 'Logg ut',
     'HEADER_TITLE_LOGIN' => 'Logg inn',
-    'HEADER_ALT_TEXT' => 'Powered by Zen Cart :: The Art of E-Commerce',
+    'HEADER_ALT_TEXT' => 'Drevet med Zen Cart :: The Art of E-Commerce',
     'HEADER_SALES_TEXT' => '<h1>Salgsmelding kan skrives her</h1>',
     'HEADER_LOGO_WIDTH' => '200px',
     'HEADER_LOGO_HEIGHT' => '70px',
