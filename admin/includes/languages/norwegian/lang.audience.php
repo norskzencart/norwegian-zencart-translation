99ebbbc0ff3e497c8b2c7f8fa59407e6ffc471d6
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2023-12-28 18:16:46 +0200

    Delete admin/includes/languages/norwegian/lang.audience.php

--- admin/includes/languages/norwegian/lang.audience.php
+++
@@ -1,9 +0,0 @@
-<?php
-$define = [
-    'TEXT_SELECT_AN_OPTION' => 'Velg',
-    'TEXT_SELECT_CUSTOMER' => 'Velg en kunde',
-    'TEXT_ALL_CUSTOMERS' => 'Til alle kunder',
-    'TEXT_NEWSLETTER_CUSTOMERS' => 'Til alle nyhetsbrevabonnenter',
-];
-
-return $define;
