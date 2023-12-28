dd09ebe4e339b6b85ebf983255a08c404bec0d56
syntaxerror <syntaxerror@users.noreply.github.com>
2023-12-28 18:44:47 +0200

    Removed deprecated strings Added header and updates

--- admin/includes/languages/norwegian/lang.currencies.php
+++ admin/includes/languages/norwegian/lang.currencies.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 09 New in v1.5.8-alpha $
+*/
+
 $define = [
     'HEADING_TITLE' => 'Valutaer',
     'TABLE_HEADING_CURRENCY_NAME' => 'Valuta',
     'TABLE_HEADING_CURRENCY_CODES' => 'Kode',
     'TABLE_HEADING_CURRENCY_VALUE' => 'Verdi',
-    'TABLE_HEADING_ACTION' => 'Valg',
-    'TEXT_INFO_EDIT_INTRO' => 'Gjør nødvendige endringer',
     'TEXT_INFO_CURRENCY_TITLE' => 'Tittel:',
     'TEXT_INFO_CURRENCY_CODE' => 'Kode:',
     'TEXT_INFO_CURRENCY_SYMBOL_LEFT' => 'Symbol venstre:',
@@ -21,7 +26,7 @@ $define = [
     'TEXT_INFO_HEADING_NEW_CURRENCY' => 'Ny valuta',
     'TEXT_INFO_HEADING_EDIT_CURRENCY' => 'Rediger valuta',
     'TEXT_INFO_HEADING_DELETE_CURRENCY' => 'Slett valuta',
-    'TEXT_INFO_SET_AS_DEFAULT' => TEXT_SET_DEFAULT.' (krever en manuell oppdatering av valutaverdier)',
+    'TEXT_INFO_SET_AS_DEFAULT' => 'Angi som standard (krever en manuell oppdatering av valutaverdier)',
     'ERROR_REMOVE_DEFAULT_CURRENCY' => 'Feil: Standard valuta kan ikke fjernes. Angi en annen valuta som standard og prøv igjen.',
     'ERROR_INVALID_CURRENCY_ENTRY' => 'FEIL: Informasjonen du la inn var mangelfull og ble ikke lagret. Du må angi en valutakode og et navn.',
 ];
