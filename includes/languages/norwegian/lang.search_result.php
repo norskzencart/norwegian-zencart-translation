559d2a829f212f1476eaa4165eb46bf004dd742a
syntaxerror <syntaxerror@users.noreply.github.com>
2023-12-28 21:40:43 +0200

    Adding new files, not translated yet

---
+++ includes/languages/norwegian/lang.search_result.php
@@ -0,0 +1,14 @@
+<?php
+$define = [
+    'NAVBAR_TITLE_1' => 'Search',
+    'NAVBAR_TITLE_2' => 'Search Results',
+    'HEADING_TITLE' => 'Search',
+    'HEADING_SEARCH_CRITERIA' => 'Search Criteria',
+    'ENTRY_CATEGORIES' => 'Categories:',
+    'ENTRY_MANUFACTURERS' => 'Manufacturers:',
+    'TABLE_HEADING_IMAGE' => '',
+    'TEXT_NO_PRODUCTS' => 'There is no product that matches the search criteria.',
+    'TEXT_SHOW' => 'Filter Results by:',
+];
+
+return $define;
