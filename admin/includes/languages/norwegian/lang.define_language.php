b0cbaee15a16f766e1b4fac0af7690bed600aa09
Rune Rasmussen <syntaxerror@users.noreply.github.com>
2023-12-28 18:17:02 +0200

    Delete admin/includes/languages/norwegian/lang.define_language.php

--- admin/includes/languages/norwegian/lang.define_language.php
+++
@@ -1,8 +0,0 @@
-<?php
-$define = [
-    'HEADING_TITLE' => 'Definer språk',
-    'TEXT_FILE_DOES_NOT_EXIST' => 'Fil eksisterer ikke.',
-    'ERROR_FILE_NOT_WRITEABLE' => 'Feil: Jeg kan ikke skrive til denne filen. Angi riktige brukerrettigheter på: %s',
-];
-
-return $define;
